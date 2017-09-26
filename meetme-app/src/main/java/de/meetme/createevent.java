2aaea563095ca431a7eec5f7d66049839a9fc5f3
Svenja Lebenhagen <svenja.lebenhagen@hpe.com>
2017-09-26 14:31:18 +0200

    Überarbeitung

--- meetme-app/src/main/java/de/meetme/createevent.java
+++ meetme-app/src/main/java/de/meetme/createevent.java
@@ -8,6 +8,6 @@ public class createevent extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_createevent);
+        setContentView(R.layout.createevent_layout);
     }
 }
