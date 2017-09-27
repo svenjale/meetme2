3a0deaa88c14cd5950931567903dbf29e00c9c69
nrl117 <nicolink117@gmail.com>
2017-09-27 20:18:52 +0200

    Help with Scroll

--- meetme-app/src/main/java/de/meetme/help.java
+++ meetme-app/src/main/java/de/meetme/help.java
@@ -3,14 +3,25 @@ package de.meetme;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
 
 import de.meetme.R;
 
 public class help extends Activity {
 
+    private TextView textViewHelpLogin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        textViewHelpLogin = (TextView) findViewById(R.id.textViewHelpLogin);
+
+        textViewHelpLogin.setMovementMethod(new ScrollingMovementMethod());
     }
 }
