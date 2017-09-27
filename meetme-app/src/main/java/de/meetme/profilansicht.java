d74d296d56eb1803e3480fee481232d715051a35
vkraenzle <31849692+vkraenzle@users.noreply.github.com>
2017-09-27 17:54:40 +0200

    Profilansicht hinzugefügt

---
+++ meetme-app/src/main/java/de/meetme/profilansicht.java
@@ -0,0 +1,16 @@
+package de.meetme;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.meetme.R;
+
+public class profilansicht extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilansicht);
+    }
+}
