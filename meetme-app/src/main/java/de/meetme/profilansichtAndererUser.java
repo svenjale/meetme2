3aa80e32f4cfd2c162c35415faffb1761f641b90
johannkortsch <31849571+johannkortsch@users.noreply.github.com>
2017-10-03 18:59:54 +0200

    Neue Seite: Profil eines anderen Users
    @Frontend Team: bitte Seiten gestalten, ähnlich aufgebaut wie eigene "Profilansicht", zusätzliche zwei Buttons: "Zu Kontakten hinzufügen" und "Nachricht senden"

---
+++ meetme-app/src/main/java/de/meetme/profilansichtAndererUser.java
@@ -0,0 +1,15 @@
+package de.meetme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.meetme.app.R;
+
+public class profilansichtAndererUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilansicht_anderer_user);
+    }
+}
