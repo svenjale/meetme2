072b289265d5c1af8c16f8dbd4bcf86303a8a73a
johannkortsch <31849571+johannkortsch@users.noreply.github.com>
2017-10-06 11:18:45 +0200

    Nachricht senden: WhatsApp/SMS
    Fremdes Profil: Button "Nachricht senden" führt zu eine WhatsApp Nachricht, hat der Kontakt kein WhatsApp, kann eine SMS Nachricht gesendet werden. :smiley_cat:

--- meetme-app/src/main/java/de/meetme/profilansichtAndererUser.java
+++ meetme-app/src/main/java/de/meetme/profilansichtAndererUser.java
@@ -1,6 +1,9 @@
 package de.meetme;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,13 +16,15 @@ import com.firebase.client.ValueEventListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 import android.app.Activity;
+import android.widget.Toast;
 
 import de.meetme.R;
 
-public class profilansichtAndererUser extends Activity implements View.OnClickListener{
+public class profilansichtAndererUser extends Activity implements View.OnClickListener {
 
     private static final String FIREBASE_URL = "https://smap-dhbw2.firebaseio.com";
     private String uebergebeneID;
+    String whatsappKontakt = "";
 
     private FirebaseAuth firebaseAuth;
     private Firebase databaseProfiles;
@@ -34,6 +39,7 @@ public class profilansichtAndererUser extends Activity implements View.OnClickLi
     private Button button6;
     private Button button4;
     private Button button16;
+    private Button button18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +60,7 @@ public class profilansichtAndererUser extends Activity implements View.OnClickLi
         button6 = (Button) findViewById(R.id.button6);
         button2 = (Button) findViewById(R.id.button2);
         button16 = (Button) findViewById(R.id.button16);
+        button18 = (Button) findViewById(R.id.button18);
 
         button15.setOnClickListener(this);
         button7.setOnClickListener(this);
@@ -61,13 +68,16 @@ public class profilansichtAndererUser extends Activity implements View.OnClickLi
         button6.setOnClickListener(this);
         button2.setOnClickListener(this);
         button16.setOnClickListener(this);
+        textView34.setOnClickListener(this);
+        button18.setOnClickListener(this);
+
 
         databaseProfiles = new Firebase(FIREBASE_URL).child("profiles").child(uebergebeneID);
         //Test: Toast.makeText(profilansicht.this, databaseProfiles.getKey(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onStart (){
+    public void onStart() {
         super.onStart();
         ValueEventListener profilListener = new ValueEventListener() {
             @Override
@@ -81,6 +91,7 @@ public class profilansichtAndererUser extends Activity implements View.OnClickLi
                 ((TextView) findViewById(R.id.textView37)).setText(ansicht.getVorname());
                 ((TextView) findViewById(R.id.textView39)).setText(ansicht.getRolle().trim());
                 ((TextView) findViewById(R.id.textView34)).setText(ansicht.getKontakt());
+                whatsappKontakt = ansicht.getKontakt();
             }
 
             @Override
@@ -94,8 +105,7 @@ public class profilansichtAndererUser extends Activity implements View.OnClickLi
     @Override
     public void onClick(View view) {
         if (view == button15) {
-            Intent switchregisintent = new Intent(profilansichtAndererUser.this, Profile_Activity.class); //switch zur Registrierung
-            startActivity(switchregisintent);
+
         }
         if (view == button2) {
             Intent Profil = new Intent(profilansichtAndererUser.this, profilansicht.class);
@@ -118,6 +128,35 @@ public class profilansichtAndererUser extends Activity implements View.OnClickLi
             Intent Hilfe = new Intent(profilansichtAndererUser.this, help.class);
             startActivity(Hilfe);
         }
+        if (view == button18) {
+            mitWhatsAppTeilen(button18);
+        }
+
+        if (view == textView34) {
+        // Anruf implementieren
+        }
     }
 
+    public void mitWhatsAppTeilen(View view) {
+
+        PackageManager pm = getPackageManager();
+        try {
+
+            Uri uri = Uri.parse("smsto:" + whatsappKontakt);
+            Intent waIntent = new Intent(Intent.ACTION_SENDTO, uri); // hier sendto eingefügt
+            //waIntent.setType("text/plain");
+            String text = "Hi! Ich habe dich auf Smap gefunden und würde dich gern auf einen Photowalk treffen!";
+            PackageInfo info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+            //Check if package exists or not. If not then code
+            //in catch block will be called
+            waIntent.setPackage("com.whatsapp");
+            waIntent.putExtra(Intent.EXTRA_TEXT, text);
+            startActivity(Intent.createChooser(waIntent, uri.toString()));
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "WhatsApp nicht installiert", Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+    }
 }
